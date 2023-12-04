c6ad32d7ecc0cbf5bec8aea2472fd8463f98dc6c
Yezda <145451081+yezdanuracik@users.noreply.github.com>
2023-12-04 10:10:29 +0300

    Add files via upload

---
+++ uygulama 1 815/uygulama 1 815/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace uygulama_1_815
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.txtSehirAdi = new System.Windows.Forms.TextBox();
+            this.lblSehir = new System.Windows.Forms.Label();
+            this.lbListe = new System.Windows.Forms.ListBox();
+            this.btnListe = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(223, 31);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(90, 35);
+            this.btnEkle.TabIndex = 0;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // txtSehirAdi
+            // 
+            this.txtSehirAdi.Location = new System.Drawing.Point(97, 39);
+            this.txtSehirAdi.Multiline = true;
+            this.txtSehirAdi.Name = "txtSehirAdi";
+            this.txtSehirAdi.Size = new System.Drawing.Size(107, 20);
+            this.txtSehirAdi.TabIndex = 1;
+            // 
+            // lblSehir
+            // 
+            this.lblSehir.AutoSize = true;
+            this.lblSehir.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSehir.Location = new System.Drawing.Point(16, 39);
+            this.lblSehir.Name = "lblSehir";
+            this.lblSehir.Size = new System.Drawing.Size(75, 20);
+            this.lblSehir.TabIndex = 2;
+            this.lblSehir.Text = "Şehir adı ";
+            // 
+            // lbListe
+            // 
+            this.lbListe.FormattingEnabled = true;
+            this.lbListe.Location = new System.Drawing.Point(336, 31);
+            this.lbListe.Name = "lbListe";
+            this.lbListe.Size = new System.Drawing.Size(120, 186);
+            this.lbListe.TabIndex = 3;
+            // 
+            // btnListe
+            // 
+            this.btnListe.Location = new System.Drawing.Point(510, 105);
+            this.btnListe.Name = "btnListe";
+            this.btnListe.Size = new System.Drawing.Size(90, 35);
+            this.btnListe.TabIndex = 4;
+            this.btnListe.Text = "Listele";
+            this.btnListe.UseVisualStyleBackColor = true;
+            this.btnListe.Click += new System.EventHandler(this.btnListe_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(632, 267);
+            this.Controls.Add(this.btnListe);
+            this.Controls.Add(this.lbListe);
+            this.Controls.Add(this.lblSehir);
+            this.Controls.Add(this.txtSehirAdi);
+            this.Controls.Add(this.btnEkle);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.TextBox txtSehirAdi;
+        private System.Windows.Forms.Label lblSehir;
+        private System.Windows.Forms.ListBox lbListe;
+        private System.Windows.Forms.Button btnListe;
+    }
+}
+
---
+++ uygulama 1 815/uygulama 1 815/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace uygulama_1_815
+{
+    public partial class Form1 : Form
+    {
+        ArrayList sehirler = new ArrayList();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            sehirler.Add(txtSehirAdi.Text);
+        }
+
+        private void btnListe_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < sehirler.Count; i++)
+            {
+                lbListe.Items.Add(sehirler[i]);
+            }
+        }
+    }
+}
